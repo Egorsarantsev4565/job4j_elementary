e0a9d9e8c4ee4c2f6a9db2f43441fd4628254129
Егор <egorsarantsev4565@yandex.ru>
2020-07-14 12:24:38 +0300

    4.4. Максимум из трех чисел [#312927] - 2

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        int result = first + second + third;
         return (third > second && third > first) ? third : (second > first) ? second : first;
     }
 
