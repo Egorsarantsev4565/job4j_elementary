ba07227ddc3c8ad4a3b47577b4bb256c7d1195af
Егор <egorsarantsev4565@yandex.ru>
2020-07-15 14:12:55 +0300

    5.8. Именование классов и переменных [#313026]

--- src/ru/job4j/loop/Mortgageru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgageru/job4j/loop/Mortgage.java
@@ -1,4 +1,4 @@
-package ru.job4j.loop.Mortgage;
+package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
--- test/ru/job4j/loop/Mortgageru/job4j/loop/MortgageTest.java
+++ test/ru/job4j/loop/Mortgageru/job4j/loop/MortgageTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.loop.Mortgage;
+package ru.job4j.loop;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
@@ -8,19 +8,19 @@ public class MortgageTest {
 
     @Test
     public void when1Year() {
-        int year = ru.job4j.loop.Mortgage.Mortgage.year(1000, 1200, 1);
+        int year = ru.job4j.loop.Mortgage.year(1000, 1200, 1);
         assertThat(year, is(1));
     }
 
     @Test
     public void when2Year() {
-        int year = ru.job4j.loop.Mortgage.Mortgage.year(100, 120, 50);
+        int year = ru.job4j.loop.Mortgage.year(100, 120, 50);
         assertThat(year, is(2));
     }
 
     @Test
     public void when4Year() {
-        int year = ru.job4j.loop.Mortgage.Mortgage.year(100, 70, 50);
+        int year = ru.job4j.loop.Mortgage.year(100, 70, 50);
         assertThat(year, is(4));
     }
 }
