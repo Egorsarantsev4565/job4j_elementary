365a2d58e280cf66f7215afefed35328c5ba65d9
Егор <egorsarantsev4565@yandex.ru>
2020-07-19 16:56:23 +0300

    6.5. Поиск индекса в диапазоне. [#312939]

--- test/ru/job4j/array/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/ru/job4j/array/FindLoopTest.java
@@ -8,7 +8,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
-        int[] input = new int[] {4, 3, 2, 5};
+        int[] input = new int[]{4, 3, 2, 5};
         int value = 5;
         int result = FindLoop.indexOf(input, value);
         int expect = 3;
@@ -17,7 +17,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas3Then0() {
-        int[] input = new int[] {1, 3, 6, 5};
+        int[] input = new int[]{1, 3, 6, 5};
         int value = 3;
         int result = FindLoop.indexOf(input, value);
         int expect = 1;
@@ -26,11 +26,41 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas4ThenNo() {
-        int[] input = new int[] {0, 5, 6, 7, 9};
+        int[] input = new int[]{0, 5, 6, 7, 9};
         int value = 4;
         int result = FindLoop.indexOf(input, value);
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
     }
 
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNoFind() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 5;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
